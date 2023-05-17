d580341524fe0a77d0f65e25abc07a8fe0815355
mohammedjabeen <mohammedjabeen07@gmail.com>
2023-05-17 16:49:13 +0530

    signout page added by jabeen

---
+++ src/test/java/jabeenDemos/Signout_page.java
@@ -0,0 +1,10 @@
+package jabeenDemos;
+
+public class Signout_page {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("signout page by jabeen");
+	}
+
+}
